2075f71516bc35eeb625430c2da049c909222e59
Andres Antonio Venialgo <63910897+aavenialgo@users.noreply.github.com>
2022-04-02 19:20:46 -0300

    implementado movimiento de criminales
    falta implementar la interaccion con civil

--- Ciber-seguridad/main.py
+++ Ciber-seguridad/main.py
@@ -12,7 +12,7 @@ from constantes import *
 class Persona(ABC):
     
     def __init__(self):
-        self.campo_de_vision = 0
+        self.campo_de_vision = 1
         self.energia = 0
         self.posicion = [0, 0]
         self.posiciones_relativas = {
@@ -29,17 +29,7 @@ class Persona(ABC):
     def set_posicion(self, nueva_pos):
         self.posicion = nueva_pos
     
-    @abstractmethod
-    def mover(self):
-        pass
-
-
-class Civil(Persona):
-    
-    def __init__(self):
-        super().__init__()
-        
-    def mover(self):
+    def mover_aleatorio(self):
         posicion_valida = False
         while not posicion_valida:
             indice = np.random(0,8)
@@ -50,12 +40,82 @@ class Civil(Persona):
                posicion_valida = True
                self.posicion = [ self.posicion[0]+pos_rel[0],
                              self.posicion[1]+pos_rel[1] ]
+               
+    
+    def buscar(self, perseguidos):
+        pass
+    
+    # def mover(self, pos_x, pos_y):
+    #     self.posicion=[pos_x,pos_y]
+
+class Civil(Persona):
+    
+    def __init__(self):
+        super().__init__()
+        
+   
              
             
 class Policia(Persona):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.posicion = [np.random.randint(0,FILAS_BARRIO),
+                         np.random.randint(0,COLUMNAS_BARRIO)]        
+        
+    def buscar(self):
+        pass
+        
 
 class Criminal(Persona):
-    pass
+    super().__init__()
+    self.posicion = [np.random.randint(0,FILAS_BARRIO),
+                     np.random.randint(0,COLUMNAS_BARRIO)]
+    
+    def mover(self, p_civiles):
+        busqueda = self.buscar(p_civiles)
+        if busqueda == None:
+            self.mover_aleatorio()
+        else:
+            self.posicion = busqueda # absoluta 
+            
+                
+        
+    def buscar(self,p_civiles):
+        respuesta = None
+        pos_x = self.posicion[0]
+        pos_y = self.posicion[1]
+        civil_mas_cercano_x = np.inf
+        civil_mas_cercano_y = np.inf
+        for civil in p_civiles:
+            pf = civil.posicion[0]
+            pc = civil.posicion[1]
+            dx = self.posicion[0] - pf
+            dy = self.posicion[1] - pc
+            
+            if campo_de_vision >= abs(dx) and campo_de_vision >= abs(dy):
+                
+                if abs(dx) < abs(civil_mas_cercano_x) and abs(dy) < abs(civil_mas_cercano_y):
+                    if abs(dx) > abs(dy):
+                        pos_x = self.posicion[0] + dx/abs(dx)
+                        pos_y = self.posicion[1]
 
+                    elif abs(dx) < abs(dy):
+                        pos_x = self.posicion[0]
+                        pos_y = self.posicion[1] + dy/abs(dy)
+                    else:
+                        pos_x = self.posicion[0] + dx/abs(dx)
+                        pos_y = self.posicion[1] + dy/abs(dy)
+                    civil_mas_cercano_x = dx
+                    civil_mas_cercano_y = dy
+                    
+                respuesta = [pos_x,pos_y]
+                
+        return respuesta
+                    
+                   
+            
+            
+            
+        
+        
 
