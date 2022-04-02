90fe02efb0a83f8ad12ecd72ff4afbca9bd08fa0
Andres Antonio Venialgo <63910897+aavenialgo@users.noreply.github.com>
2022-04-02 09:39:55 -0300

    agregamos las clases

---
+++ Ciber-seguridad/Barrio.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 00:47:59 2022
+
+@author: Andres Venialgo
+"""
+
--- Ciber-seguridad/main.py
+++ Ciber-seguridad/main.py
@@ -4,5 +4,37 @@ Created on Sat Apr  2 00:00:22 2022
 
 """
 
+from abc import ABC, abstractmethod
+import numpy as np
+
+class Persona(ABC):
+    
+    def __init__(self):
+        self.campo_de_vision = 0
+        self.energia = 0
+        self.posicion = [0, 0]
+    
+    def set_posicion(self, nueva_pos):
+        self.posicion = nueva_pos
+    
+    @abstractmethod
+    def mover(self):
+        pass
+
+
+class Civil(Persona):
+    
+    def __init__(self):
+        super().__init__()
+        
+    def mover(self):
+        
+    
+
+class Policia:
+    pass
+
+class Criminal:
+    pass
 
 
