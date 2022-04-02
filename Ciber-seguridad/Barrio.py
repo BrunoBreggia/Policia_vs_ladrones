3dbba4ba4add2b668c9c44f3d4e2f20281813166
Andres Antonio Venialgo <63910897+aavenialgo@users.noreply.github.com>
2022-04-02 17:55:15 -0300

    funcion mover de civil

--- Ciber-seguridad/Barrio.py
+++ Ciber-seguridad/Barrio.py
@@ -4,4 +4,11 @@ Created on Sat Apr  2 00:47:59 2022
 
 @author: Andres Venialgo
 """
-
+from constantes import * 
+class Barrio:
+    
+    def __init__(self):
+        self.civiles = [Civil() for i in range(CIVILES_INICIALES)]
+        self.policias = [Policia() for i in range(POLICIAS_INICIALES)]
+        self.criminales = [Criminales() for i in range(CRIMINASLES_INICIALES)]
+        
--- Ciber-seguridad/main.py
+++ Ciber-seguridad/main.py
@@ -6,6 +6,8 @@ Created on Sat Apr  2 00:00:22 2022
 
 from abc import ABC, abstractmethod
 import numpy as np
+from constantes import * 
+
 
 class Persona(ABC):
     
@@ -13,6 +15,16 @@ class Persona(ABC):
         self.campo_de_vision = 0
         self.energia = 0
         self.posicion = [0, 0]
+        self.posiciones_relativas = {
+            0:[-1,0], 
+            1:[-1,1],
+            2:[0,1],
+            3:[1,1],
+            4:[1,0],
+            5:[1,-1],
+            6:[0,-1],
+            7:[-1,-1]
+            }
     
     def set_posicion(self, nueva_pos):
         self.posicion = nueva_pos
@@ -28,13 +40,22 @@ class Civil(Persona):
         super().__init__()
         
     def mover(self):
-        
-    
-
-class Policia:
+        posicion_valida = False
+        while not posicion_valida:
+            indice = np.random(0,8)
+            pos_rel = self.posiciones_relativas[indice]
+            pf = self.posicion[0]+ pos_rel
+            pc = self.posicion[1]+ pos_rel
+            if (0 <= pf < FILAS_BARRIO) and (0 <= pc < COLUMNAS_BARRIO):
+               posicion_valida = True
+               self.posicion = [ self.posicion[0]+pos_rel[0],
+                             self.posicion[1]+pos_rel[1] ]
+             
+            
+class Policia(Persona):
     pass
 
-class Criminal:
+class Criminal(Persona):
     pass
 
 
